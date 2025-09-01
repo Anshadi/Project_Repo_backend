ebcfab284c228201adc232c607f20e131a07e860
Anshadi <125490351+Anshadi@users.noreply.github.com>
2025-09-01 20:34:05 +0530

    Update ProductController.java

--- src/main/java/com/voice/shopping/controller/ProductController.java
+++ src/main/java/com/voice/shopping/controller/ProductController.java
@@ -14,7 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/search")
 @RequiredArgsConstructor
-@CrossOrigin(origins = { "http://localhost:*", "http://127.0.0.1:*" })
+@CrossOrigin(origins = { "https://project-repo-0.onrender.com", "http://localhost:*", "http://127.0.0.1:*" })
 public class ProductController {
 
     private final ProductService productService;
