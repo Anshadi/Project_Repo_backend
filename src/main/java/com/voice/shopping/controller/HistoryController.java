c0b0b97eb009e1fd94b0aec9286e9ae82eb57cdc
Anshadi <125490351+Anshadi@users.noreply.github.com>
2025-09-01 20:33:44 +0530

    Update HistoryController.java

--- src/main/java/com/voice/shopping/controller/HistoryController.java
+++ src/main/java/com/voice/shopping/controller/HistoryController.java
@@ -13,7 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/history")
 @RequiredArgsConstructor
-@CrossOrigin(origins = { "http://localhost:*", "http://127.0.0.1:*" })
+@CrossOrigin(origins = { "https://project-repo-0.onrender.com", "http://localhost:*", "http://127.0.0.1:*" })
 public class HistoryController {
 
     private final PurchaseHistoryRepository purchaseHistoryRepository;
