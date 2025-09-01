765ba91b7359eebea155c888220698371c79548b
Anshadi <125490351+Anshadi@users.noreply.github.com>
2025-09-01 20:29:49 +0530

    Update ShoppingListController.java

--- src/main/java/com/voice/shopping/controller/ShoppingListController.java
+++ src/main/java/com/voice/shopping/controller/ShoppingListController.java
@@ -18,7 +18,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/list")
 @RequiredArgsConstructor
-@CrossOrigin(origins = { "http://localhost:*", "http://127.0.0.1:*" })
+@CrossOrigin(origins = { "https://project-repo-0.onrender.com", "http://localhost:*", "http://127.0.0.1:*" })
 public class ShoppingListController {
 
     private final ShoppingListService shoppingListService;
